bf47361b69724c67a1f7933222c9f8b9ad041f62
Doston Hamrakulov <doston.hamrakulov@gmail.com>
2017-06-02 23:27:29 +0300

    I added more countries to the array

--- game.js
+++ game.js
@@ -17,35 +17,56 @@ function Redirect() {
 }
 function initializeSecond() {
     document.getElementById('birinchi').innerHTML = "Player: " + name;
+    document.getElementById('title').innerHTML = name + " in game!";
     numQuestion = 1;
     score = 0;
     questions();
 }
 function questions(){
-    var countries = [
-      "Uzbekistan", "Chine", "Nepal",
-      "Egypt", "Congo", "Algeria",
-      "Brazil", "Chili", "Urugvay",
-      "USA", "Canada", "Mexico",
-      "Germany", "Italy", "UK"];
+
+    var countries1 = [
+      "China", "Uzbekistan", "South Korea", "Uzbekistan", "Uzbekistan",
+      "Bangladesh", "Iran", "Nepal", "Pakistan", "Turkey",
+      "Uzbekistan", "Russian Federation", "United Arab Emirates", "Vietnam", "Singapore",
+
+      "Namibia", "Ghana", "Egypht", "Congo", "Algeria",
+      "Angola", "Cameroon", "Gambia", "Kenya", "Libya",
+      "Liberia", "Ethiopia", "Nigeria", "Niger", "Somalia",
+
+      "Brazil", "Chili", "Argentina", "Peru", "Ecuador",
+      "Bolivia", "Guyana", "Paraguay", "Uruguay", "Venezuela",
+
+      "Honduras", "Dominica", "USA", "Cuba", "Mexico",
+      "Canada", "Panama",
+
+      "Malta", "Liechtenstein", "Iceland", "Georgia", "Cyrpus",
+      "Albania", "Bulgaria", "Czech Republic", "Finland", "Hungary",
+      "Luxembourg", "Moldova", "Norway"
+    ];
+    // var countries = [
+    //   "Uzbekistan", "Chine", "Nepal",
+    //   "Egypt", "Congo", "Algeria",
+    //   "Brazil", "Chili", "Urugvay",
+    //   "USA", "Canada", "Mexico",
+    //   "Germany", "Italy", "UK"];
     var rand = Random();
 
     document.getElementById('question').innerHTML = "Question N:" + numQuestion
-    + " In which continent is " + countries[rand] + " located?"
-    if (rand < 3) {
+    + " In which continent is " + countries1[rand] + " located?";
+    if (rand < 15) {
       choice = 1;
-    } else if (rand > 2 && rand < 6) {
+    } else if (rand > 14 && rand < 30) {
       choice = 2;
-    } else if (rand > 5 && rand < 9) {
+    } else if (rand > 29 && rand < 40) {
       choice = 3;
-    } else if (rand > 8 && rand < 12) {
+    } else if (rand > 39 && rand < 47) {
       choice = 4;
     } else {
       choice = 5;
     }
 }
 function Random(){
-  return Math.floor(Math.random()*15);
+  return Math.floor(Math.random()*59);
 }
 function check(selected){
 
@@ -89,6 +110,7 @@ function mUp(obj){
 
 function initializeDetails(){
   document.getElementById('birinchi').innerHTML = "Player: " + name;
+  document.getElementById('title').innerHTML = name + " in game!";
 }
 
 // for validation
